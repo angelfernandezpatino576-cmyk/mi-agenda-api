403685e8dbfc73611b9fea0b2a845cc7cbcaf586
AngelFP <angelfernandezpatino576@gmail.com>
2026-01-23 21:25:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
 from fastapi import FastAPI
-import uvicorn
-
 app = FastAPI()
-
 @app.get("/")
-def home():
-    return {"status": "Servidor Agente 2026 Online"}
-
-@app.get("/tareas/")
-def leer_tareas():
-    return [{"id": 1, "titulo": "Conexión exitosa con Koyeb", "estado": "ok"}]
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+def root(): return {"status": "Servidor activo"}
