944cfef46cd574c4de9738f20eb73d2364b60738
AngelFP <angelfernandezpatino576@gmail.com>
2026-01-24 16:25:37 -0500

    Update microfono.py

--- CORE/microfono.py
+++ CORE/microfono.py
@@ -3,6 +3,5 @@ import speech_recognition as sr
 def escuchar():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("🎙️ Escuchando...")
         audio = r.listen(source)
         return r.recognize_google(audio, language="es-ES")
