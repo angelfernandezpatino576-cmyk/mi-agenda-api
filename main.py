93924b7850f422ee6f8aac74b1bb6281babc69c5
AngelFP <angelfernandezpatino576@gmail.com>
2026-01-23 21:27:05 -0500

    Update root function to home and change status message

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
+import uvicorn
+
 app = FastAPI()
+
 @app.get("/")
-def root(): return {"status": "Servidor activo"}
+def home():
+    return {"status": "Servidor Agente 2026 Online"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
